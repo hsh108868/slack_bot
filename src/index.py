451894e4aa2ba33ea3f11a942e56ce6f8f160342
hsh108868 <pusrty@gmail.com>
2022-05-26 03:50:49 +0900

    index

--- src/index.py
+++ src/index.py
@@ -1,5 +1,19 @@
+from concurrent.futures import process
+from email import message
+from lib2to3.pgen2 import token
 import os
+import boto3
+
+s3 = boto3.client('s3', region_name='ap-northwest-2')
 
 
-def lambda_handler(event, context):
-    messeage = ''
+def awsLambdaReceiver():
+    signingSecret = process.env.SLACK_SIGNING_SECRET
+    return {
+        signingSecret: signingSecret
+    }
+
+
+def app():
+    return{token: process.env.SLACK_BOT_TOKEN,
+           "receiver": awsLambdaReceiver}
