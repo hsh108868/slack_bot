59d76e6f44cd9bee0a9afa70a21f834bece08e7f
hsh108868 <pusrty@gmail.com>
2022-06-18 23:02:07 +0900

    modify index.py

--- src/index.py
+++ src/index.py
@@ -8,6 +8,7 @@ s3 = boto3.client('s3', region_name='ap-northwest-2')
 
 
 def lambda_handler(event, context) -> str:
+    json_region = os.environ['ap-northwest-2']
     logging.info(json.dumps(event))
 
     if "challenge" in event:
